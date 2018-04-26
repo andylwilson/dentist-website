ee7c4c9da75c84106f789b821df09c04d243980e
jetsetrez <38418756+jetsetrez@users.noreply.github.com>
2018-04-25 20:37:27 -0700

    Initial upload of web120 project

---
+++ flowchart.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php"?>
+<!--END header.php include here-->
+
+<!--responsive tables coming soon!-->
+ <p>We'll attempt iframing in a google doc here that shows our wireframe and flowchart</p>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Provide 2 resource links to wireframing resources I like</h3>
+    <ol>
+        <li>this is a wireframe or flowchart article (link)</li>
+        <li>this is a wireframe or flowchart application (link)</li>
+    </ol>
+</aside>
+<!-- END RIGHT COL -->
+
+<!--footer.php include starts here-->
+<?php include "includes/footer.php"?>
